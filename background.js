6cb377a38aee23b3fe8cff97deaf3a90c8d0427e
gmorton <gmorton@eticainc.com>
2020-12-13 14:41:02 -1000

    First commit

---
+++ background.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var host = "https://old.reddit.com";
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) {
+         return {redirectUrl: host + details.url.match(/^https?:\/\/[^\/]+([\S\s]*)/)[1]};
+    },
+    {
+        urls: [
+            "*://reddit.com/*",
+            "*://www.reddit.com/*"
+        ],
+        types: ["main_frame", "sub_frame", "stylesheet", "script", "image", "object", "xmlhttprequest", "other"]
+    },
+    ["blocking"]
+);
